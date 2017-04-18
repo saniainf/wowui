3f87d5aa220b2125498f6a14f6318b8b432e3e99
inf <saniainf@gmail.com>
2017-04-18 15:46:29 +0700

    525

---
+++ Bartender4.lua
@@ -0,0 +1,245 @@
+
+Bartender4DB = {
+	["namespaces"] = {
+		["ActionBars"] = {
+			["profiles"] = {
+				["Исендур - Азурегос"] = {
+					["actionbars"] = {
+						{
+							["rows"] = 2,
+							["skin"] = {
+								["Zoom"] = true,
+							},
+							["buttons"] = 8,
+							["version"] = 3,
+							["position"] = {
+								["y"] = 90,
+								["x"] = -253,
+								["point"] = "BOTTOM",
+							},
+							["padding"] = 6,
+							["visibility"] = {
+								["possess"] = true,
+								["vehicleui"] = true,
+							},
+						}, -- [1]
+						{
+							["rows"] = 2,
+							["skin"] = {
+								["Zoom"] = true,
+							},
+							["buttons"] = 8,
+							["version"] = 3,
+							["position"] = {
+								["y"] = 90,
+								["x"] = 82,
+								["point"] = "BOTTOM",
+							},
+							["padding"] = 6,
+							["visibility"] = {
+								["possess"] = true,
+								["overridebar"] = false,
+							},
+						}, -- [2]
+						{
+							["rows"] = 12,
+							["version"] = 3,
+							["position"] = {
+								["y"] = 134.718353271484,
+								["x"] = -252.67529296875,
+								["point"] = "RIGHT",
+							},
+							["padding"] = 5,
+						}, -- [3]
+						{
+							["rows"] = 12,
+							["enabled"] = false,
+							["version"] = 3,
+							["position"] = {
+								["y"] = 132.084503173828,
+								["x"] = -164.208618164063,
+								["point"] = "RIGHT",
+							},
+							["padding"] = 5,
+						}, -- [4]
+						{
+							["enabled"] = false,
+							["version"] = 3,
+							["position"] = {
+								["y"] = 83,
+								["x"] = 3,
+								["point"] = "BOTTOM",
+							},
+							["padding"] = 6,
+						}, -- [5]
+						{
+							["enabled"] = false,
+							["version"] = 3,
+							["position"] = {
+								["y"] = 83,
+								["x"] = -510,
+								["point"] = "BOTTOM",
+							},
+							["padding"] = 6,
+						}, -- [6]
+						{
+						}, -- [7]
+						{
+						}, -- [8]
+						nil, -- [9]
+						{
+						}, -- [10]
+					},
+				},
+			},
+		},
+		["LibDualSpec-1.0"] = {
+		},
+		["ExtraActionBar"] = {
+			["profiles"] = {
+				["Исендур - Азурегос"] = {
+					["position"] = {
+						["y"] = -176.169494628906,
+						["x"] = 252.507705688477,
+						["point"] = "LEFT",
+					},
+					["version"] = 3,
+				},
+			},
+		},
+		["ZoneAbilityBar"] = {
+			["profiles"] = {
+				["Исендур - Азурегос"] = {
+					["position"] = {
+						["y"] = -179.108306884766,
+						["x"] = 170.604644775391,
+						["point"] = "LEFT",
+					},
+					["version"] = 3,
+				},
+			},
+		},
+		["MicroMenu"] = {
+			["profiles"] = {
+				["Исендур - Азурегос"] = {
+					["position"] = {
+						["y"] = -207.646118164063,
+						["x"] = -318.898803710938,
+						["point"] = "TOPRIGHT",
+						["scale"] = 1,
+					},
+					["version"] = 3,
+					["padding"] = -2,
+				},
+			},
+		},
+		["XPBar"] = {
+			["profiles"] = {
+				["Исендур - Азурегос"] = {
+					["position"] = {
+						["y"] = 10,
+						["x"] = 396.496673583984,
+						["point"] = "LEFT",
+					},
+					["version"] = 3,
+				},
+			},
+		},
+		["APBar"] = {
+			["profiles"] = {
+				["Исендур - Азурегос"] = {
+					["position"] = {
+						["y"] = 10,
+						["x"] = 396.496673583984,
+						["point"] = "LEFT",
+					},
+					["version"] = 3,
+				},
+			},
+		},
+		["BlizzardArt"] = {
+			["profiles"] = {
+				["Исендур - Азурегос"] = {
+					["position"] = {
+						["y"] = 47,
+						["x"] = -512,
+						["point"] = "BOTTOM",
+					},
+					["version"] = 3,
+				},
+			},
+		},
+		["Vehicle"] = {
+			["profiles"] = {
+				["Исендур - Азурегос"] = {
+					["version"] = 3,
+					["position"] = {
+						["y"] = -185.736465454102,
+						["x"] = 91.1361694335938,
+						["point"] = "LEFT",
+					},
+				},
+			},
+		},
+		["BagBar"] = {
+			["profiles"] = {
+				["Исендур - Азурегос"] = {
+					["onebag"] = true,
+					["position"] = {
+						["y"] = 36.0000038146973,
+						["x"] = -75.37841796875,
+						["point"] = "BOTTOMRIGHT",
+					},
+					["version"] = 3,
+				},
+			},
+		},
+		["StanceBar"] = {
+			["profiles"] = {
+				["Исендур - Азурегос"] = {
+					["position"] = {
+						["y"] = -15.0000305175781,
+						["x"] = -82.4999847412109,
+						["point"] = "CENTER",
+					},
+					["version"] = 3,
+				},
+			},
+		},
+		["PetBar"] = {
+			["profiles"] = {
+				["Исендур - Азурегос"] = {
+					["enabled"] = false,
+					["version"] = 3,
+					["position"] = {
+						["y"] = 116,
+						["x"] = -460,
+						["point"] = "BOTTOM",
+					},
+				},
+			},
+		},
+		["RepBar"] = {
+			["profiles"] = {
+				["Исендур - Азурегос"] = {
+					["position"] = {
+						["y"] = 7.99996948242188,
+						["x"] = 398.996765136719,
+						["point"] = "LEFT",
+					},
+					["version"] = 3,
+				},
+			},
+		},
+	},
+	["profileKeys"] = {
+		["Исендур - Азурегос"] = "Исендур - Азурегос",
+	},
+	["profiles"] = {
+		["Исендур - Азурегос"] = {
+			["focuscastmodifier"] = false,
+			["blizzardVehicle"] = true,
+			["outofrange"] = "hotkey",
+		},
+	},
+}
